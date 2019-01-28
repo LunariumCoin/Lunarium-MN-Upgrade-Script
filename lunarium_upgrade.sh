15c45fd6d7c712b0c18a5d61ebfc2c6eaeb47126
RealHoneyBadger <38737396+RealHoneyBadger@users.noreply.github.com>
2019-01-28 10:54:17 +0100

    Create lunarium_upgrade.sh

---
+++ lunarium_upgrade.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+LUNARIUM_LATEST_RELEASE="https://github.com/LunariumCoin/lunarium/releases/download/v1.0.2/lunarium-1.0.2-x86_64-linux-gnu.tar.gz"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+function checks() {
+if [[ $(lsb_release -d) != *16.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
+}
+
+function ask_yes_or_no() {
+  read -p "$1 ([Y]es or [N]o | ENTER): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
+function copy_lunarium_binaries(){
+  cd
+  wget $LUNARIUM_LATEST_RELEASE
+  tar -xzf `basename $LUNARIUM_LATEST_RELEASE` --strip-components=2
+  cp -fr lunarium-cli lunariumd lunarium-tx lunarium-qt /usr/local/bin
+  chmod 755 /usr/local/bin/lunarium*
+}
+
+function upgrade() {
+  echo -e "${GREEN}This script can only be used to upgrade Lunarium Masternodes to version 1.1 that was previously installed using the offical shell script.${NC}"
+  if [[ "yes" == $(ask_yes_or_no "Do you wish to upgrade?") ]]
+    then
+      systemctl stop lunarium.service
+      copy_lunarium_binaries
+      systemctl start lunarium.service
+      echo -e "${GREEN}If all went well with the upgrade please wait at least five minutes and then re-start the MasterNode from the hot wallet.${NC}"
+  else
+    echo -e "Script terminated and Masternode not upgraded to latest version!"
+    exit 1
+  fi
+}
+
+##### Main #####
+checks
+upgrade
